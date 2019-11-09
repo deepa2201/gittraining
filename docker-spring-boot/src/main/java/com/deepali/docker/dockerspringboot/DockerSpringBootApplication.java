d73e91c2b9cb5a54b1881c9b863c2e70d16c3470
deepa2201 <56442276+deepa2201@users.noreply.github.com>
2019-11-08 19:59:08 -0500

    Update DockerSpringBootApplication.java

--- docker-spring-boot/src/main/java/com/deepali/docker/dockerspringboot/DockerSpringBootApplication.java
+++ docker-spring-boot/src/main/java/com/deepali/docker/dockerspringboot/DockerSpringBootApplication.java
@@ -3,10 +3,12 @@ package com.deepali.docker.dockerspringboot;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
 
 @SpringBootApplication
 @ComponentScan(basePackages={"com.deepali.docker.dockerspringboot"})
-public class DockerSpringBootApplication {
+public class DockerSpringBootApplication extends SpringBootServletInitializer{
 
 	public static void main(String[] args) {
 		SpringApplication.run(DockerSpringBootApplication.class, args);
